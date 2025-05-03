e3b89f4c4a7699efeef39310b4b32b324a3cfb8a
Aidai Mashrapova <aidaimashrapova8@mail.ru>
2025-05-03 13:46:51 +0600

    User's Register and Login functions created

---
+++ api/types.d.ts
@@ -0,0 +1,7 @@
+export interface UserFields {
+    username: string;
+    password: string;
+    token: string;
+    displayName: string;
+    phoneNumber: string;
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import {Container, CssBaseline, Typography} from "@mui/material";
+import AppToolbar from "./components/UI/AppToolbar/AppToolbar.tsx";
+import {Route, Routes} from "react-router-dom";
+import {ToastContainer} from "react-toastify";
+import Register from "./features/users/Register.tsx";
+import Login from "./features/users/Login.tsx";
+// import ProtectedRoute from "./components/UI/ProtectedRoute/ProtectedRoute.tsx";
+// import {useAppSelector} from "./app/hooks.ts";
+// import {selectUser} from "./features/users/usersSlice.ts";
+
+
+const App = () => {
+    // const user = useAppSelector(selectUser);
+
+    return (
+        <>
+            <CssBaseline/>
+            <ToastContainer/>
+            <header>
+                <AppToolbar/>
+            </header>
+            <main>
+                <Container maxWidth="xl">
+                    <Routes>
+                        {/*<Route path="/" element={<Products/>}/>*/}
+                        <Route path="/register" element={<Register/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                        {/*<Route path="/posts/:id" element={<FullProduct>}/>*/}
+
+                        {/*<Route path="/products/new" element={*/}
+                        {/*    <ProtectedRoute isAllowed={Boolean(user)}><NewProduct/></ProtectedRoute>*/}
+                        {/*}/>*/}
+
+                        <Route path="*" element={<Typography variant="h4">Not found page</Typography>}/>
+                    </Routes>
+                </Container>
+            </main>
+        </>
+    )
+};
+
+export default App
